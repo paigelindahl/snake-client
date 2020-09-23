d31253d10670da276b97e7957b412e1475a7cfa9
paigelindahl <paigemarlindahl@gmail.com>
2020-09-23 20:30:54 +0000

    added name and connection message

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require('net');
+
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: PML');
+  })
+  conn.on('data', (data) => {
+    console.log('Incoming: ', data);
+  
+  });
+  
+  // })
+
+  return conn;
+
+  
+};
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,23 +1,6 @@
-const net = require('net');
+// const connect = require('./client');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', (data) => {
-    console.log('Incoming: ', data);
-  });
-  
-  // })
-
-  return conn;
-}
+const {connect} = require('./client');
 
 console.log('Connecting ...');
 connect();
