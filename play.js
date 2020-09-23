33b7127fcbc882f31db6347ae5745f9760c0a0d5
paigelindahl <paigemarlindahl@gmail.com>
2020-09-23 21:46:46 +0000

    added handleUserInput function

--- play.js
+++ play.js
@@ -2,5 +2,25 @@
 
 const {connect} = require('./client');
 
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+  return stdin;
+};
+
+const handleUserInput = function(char) {
+  if (char === 'x') {
+    process.exit();
+  };
+
+};
+
+
+setupInput();
 console.log('Connecting ...');
 connect();
